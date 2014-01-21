d07e99f965475bdef1f5e7d5e0a26fc605ea3c95
Victor <victor@suumit.com>
2014-01-21 18:26:29 +0100

    Add the first HACK sample

---
+++ www/hello/index.php
@@ -0,0 +1,17 @@
+<?hh
+
+require "/vagrant/www/xhp/php-lib/init.php";
+
+$hello = "Hello HACK!";
+
+echo <html>
+    <head>
+        <title>{$hello}!</title>
+    </head>
+    <body>
+        <h1>{$hello}</h1>
+    </body>
+</html>;
+
+
+
