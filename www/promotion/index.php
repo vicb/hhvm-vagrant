9260b4b9cef0a8e9b7d1e9af87ed1ffd6dba1508
Victor <victor@suumit.com>
2014-01-26 11:04:53 +0100

    Add constructor argument promotion sample

---
+++ www/promotion/index.php
@@ -0,0 +1,31 @@
+<?hh
+
+class PHPClass {
+    public $pub;
+    protected $pro;
+    private $pri;
+
+    public function __construct($pub, $pro, $pri) {
+        $this->pub = $pub;
+        $this->pro = $pro;
+        $this->pri = $pri;
+    }
+}
+
+// object(PHPClass)#6 (3) {
+//   ["pub"]=> string(3) "pub"
+//   ["pro":protected]=> string(3) "pro"
+//   ["pri":"PHPClass":private]=> string(3) "pri"
+//}
+var_dump(new PHPClass('pub', 'pro', 'pri'));
+
+class HHClass {
+    public function __construct(public $pub, protected $pro, private $pri) {}
+}
+
+// object(HHClass)#6 (3) {
+//   ["pub"]=> string(3) "pub"
+//   ["pro":protected]=> string(3) "pro"
+//   ["pri":"HHClass":private]=> string(3) "pri"
+// }
+var_dump(new HHClass('pub', 'pro', 'pri'));
